7dc453b3579959e6465c137ec408adddb73215e7
Mathias Gottschlag <mgottschlag@gmail.com>
2010-08-21 16:33:38 +0200

    Cleaned the HelloWorld example.

--- Tutorials/HelloWorld/HelloWorld.cpp
+++ Tutorials/HelloWorld/HelloWorld.cpp
@@ -22,61 +22,6 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "CoreRender.hpp"
 
 #include <iostream>
-#include <GL/glfw.h>
-
-float vertices[24 * 8] = {
-	// Front
-	1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0,
-	-1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0,
-	-1.0, -1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0,
-	1.0, -1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0,
-	// Back
-	1.0, 1.0, -1.0, 0.0, 0.0, -1.0, 1.0, 1.0,
-	1.0, -1.0, -1.0, 0.0, 0.0, -1.0, 1.0, 0.0,
-	-1.0, -1.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0,
-	-1.0, 1.0, -1.0, 0.0, 0.0, -1.0, 0.0, 1.0,
-	// Top
-	1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0,
-	1.0, 1.0, -1.0, 0.0, 1.0, 0.0, 1.0, 0.0,
-	-1.0, 1.0, -1.0, 0.0, 1.0, 0.0, 0.0, 0.0,
-	-1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0,
-	// Bottom
-	1.0, -1.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0,
-	-1.0, -1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0,
-	-1.0, -1.0, -1.0, 0.0, 1.0, 0.0, 0.0, 0.0,
-	1.0, -1.0, -1.0, 0.0, 1.0, 0.0, 1.0, 0.0,
-	// Left
-	1.0, 1.0, 1.0, -1.0, 0.0, 0.0, 1.0, 1.0,
-	1.0, -1.0, 1.0, -1.0, 0.0, 0.0, 0.0, 1.0,
-	1.0, -1.0, -1.0, -1.0, 0.0, 0.0, 0.0, 0.0,
-	1.0, 1.0, -1.0, -1.0, 0.0, 0.0, 1.0, 0.0,
-	// Right +
-	-1.0, 1.0, 1.0, -1.0, 0.0, 0.0, 1.0, 1.0,
-	-1.0, 1.0, -1.0, -1.0, 0.0, 0.0, 1.0, 0.0,
-	-1.0, -1.0, -1.0, -1.0, 0.0, 0.0, 0.0, 0.0,
-	-1.0, -1.0, 1.0, -1.0, 0.0, 0.0, 0.0, 1.0,
-};
-
-unsigned short indices[36] = {
-	// Front
-	0, 1, 2,
-	0, 2, 3,
-	// Back
-	4, 5, 6,
-	4, 6, 7,
-	// Top
-	8, 9, 10,
-	8, 10, 11,
-	// Bottom
-	12, 13, 14,
-	12, 14, 15,
-	// Left
-	16, 17, 18,
-	16, 18, 19,
-	// Right
-	20, 21, 22,
-	20, 22, 23,
-};
 
 int main(int argc, char **argv)
 {
@@ -103,29 +48,6 @@ int main(int argc, char **argv)
 
 	// Load some resources
 	cr::render::Model::Ptr model = graphics.loadModel("/models/jeep.model.xml");
-
-	cr::render::VertexBuffer::Ptr vb = graphics.createVertexBuffer();
-	vb->set(24 * 8 * sizeof(float), vertices);
-	cr::render::IndexBuffer::Ptr ib = graphics.createIndexBuffer();
-	ib->set(36 * sizeof(unsigned short), indices);
-	cr::render::Material::Ptr material = graphics.loadMaterial("/materials/Simple.material.xml");
-	// Create vertex layout
-	cr::render::VertexLayout::Ptr layout = new cr::render::VertexLayout(3);
-	layout->setElement(0, "pos", 0, 3, 0, cr::render::VertexElementType::Float, 32);
-	layout->setElement(1, "normal", 0, 3, 12, cr::render::VertexElementType::Float, 32);
-	layout->setElement(2, "texcoord", 0, 2, 24, cr::render::VertexElementType::Float, 32);
-	// Create render job
-	cr::render::RenderJob *job = new cr::render::RenderJob;
-	job->vertices = vb;
-	job->vertexcount = 24;
-	job->indices = ib;
-	job->endindex = 36;
-	job->material = material;
-	job->layout = layout;
-	// This does not work as the shader is not loaded yet
-	//job->uniforms = shader->getUniformData();
-	cr::math::Matrix4 matrix = cr::math::Matrix4::ScaleMat(cr::math::Vector3F(0.5f, 0.5f, 0.5f));
-	job->uniforms.add("worldMat") = matrix;
 	// Setup pipeline
 	cr::render::Pipeline::Ptr pipeline = new cr::render::Pipeline();
 	cr::render::RenderPass::Ptr pass = new cr::render::RenderPass("AMBIENT");
@@ -133,8 +55,8 @@ int main(int argc, char **argv)
 	graphics.addPipeline(pipeline);
 	// Wait for resources to be loaded
 	model->waitForLoading(true);
-	material->waitForLoading(true);
 	// Model render job
+	cr::math::Matrix4 matrix = cr::math::Matrix4::ScaleMat(cr::math::Vector3F(0.5f, 0.5f, 0.5f));
 	std::vector<cr::render::RenderJob*> modeljobs(model->getMeshCount());
 	for (unsigned int i = 0; i < model->getMeshCount(); i++)
 	{
@@ -153,6 +75,8 @@ int main(int argc, char **argv)
 		modeljobs[i]->indextype = batch->indextype;
 		modeljobs[i]->uniforms.add("worldMat") = matrix;
 	}
+	// Finished loading
+	graphics.getLog()->info("Starting rendering.");
 	// Render loop
 	bool stopping = false;
 	uint64_t fpstime = cr::core::Time::getSystemTime();
@@ -168,17 +92,6 @@ int main(int argc, char **argv)
 				case cr::render::InputEventType::WindowClosed:
 					stopping = true;
 					break;
-				case cr::render::InputEventType::KeyDown:
-					/*if (input.keyboard.key == cr::render::KeyCode::Up)
-						scale.y *= 1.1f;
-					if (input.keyboard.key == cr::render::KeyCode::Down)
-						scale.y /= 1.1f;
-					if (input.keyboard.key == cr::render::KeyCode::Left)
-						scale.x *= 1.1f;
-					if (input.keyboard.key == cr::render::KeyCode::Right)
-						scale.x /= 1.1f;
-					job->uniforms["scale"] = scale;*/
-					break;
 				default:
 					break;
 			}
@@ -187,7 +100,6 @@ int main(int argc, char **argv)
 		graphics.beginFrame();
 		// Render objects
 		matrix = matrix * cr::math::Quaternion(cr::math::Vector3F(0.0, 0.1, 0.0)).toMatrix();
-		job->uniforms["worldMat"] = matrix;
 		for (unsigned int i = 0; i < modeljobs.size(); i++)
 		{
 			modeljobs[i]->uniforms["worldMat"] = matrix;
@@ -210,10 +122,6 @@ int main(int argc, char **argv)
 	{
 		delete modeljobs[i];
 	}
-	delete job;
-	vb = 0;
-	ib = 0;
-	material = 0;
 	pipeline = 0;
 	pass = 0;
 	model = 0;
